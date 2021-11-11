73ed065d408e1e883d59bdae5d5f11304ec6664b
jimafisk <jimafisk@gmail.com>
2021-11-11 17:51:29 -0500

    Three green squares.

---
+++ layouts/components/squares.svelte
@@ -0,0 +1,20 @@
+<section>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+</section>
+
+<style>
+  section {
+    display: flex;
+  }
+  div {
+    height: 50px;
+    width: 50px;
+    margin: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: green;
+  }
+</style>
---
+++ layouts/content/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let title;
+
+  import Squares from '../components/squares.svelte';
+</script>
+
+<h1>{title}</h1>
+
+<Squares />
+
+<style>
+	h1 {
+		font-family: Helvetica, sans-serif;
+	}
+</style>
